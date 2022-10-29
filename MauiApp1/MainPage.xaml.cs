6c7e464db44b12241253dd6ac8f7018bbe104f79
Arash zandi <arash.zandi@live.com>
2022-10-29 22:46:50 +0330

    first commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	bool alt = false;
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		TxtMessage.HorizontalTextAlignment = alt ? TextAlignment.Start : TextAlignment.End;
+		TxtMessage.VerticalTextAlignment = alt ? TextAlignment.Start : TextAlignment.End;
+		alt = !alt;
+    }
+}
+
